808efc45bf2e59f5b6c8244623f38e4c7100fec6
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-07-23 12:27:21 +0200

    V.2.0 eBay Makeover. Modified the seed file in order to display bids and reviews.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,19 @@ user2 = User.create!(name: Faker::Name.name, email: Faker::Internet.free_email, 
 user3 = User.create!(name: Faker::Name.name, email: Faker::Internet.free_email, password: '123', password_confirmation: '123')
 
 3.times do |product|
-user1.products.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
+  user1.products.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
 end
 
 3.times do |product|
-user2.products.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
+  user2.products.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
 end
 
 3.times do |product|
-user3.products.create!(title: Faker::Lorem.words(2).join(''), description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
+  user3.products.create!(title: Faker::Lorem.words(2).join(''), description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
+end
+
+
+50.times do |review|
+  Product.find_by_id(rand(Product.first.id..Product.last.id)).reviews.create!(user: User.find_by_id(rand(User.first.id..User.last.id)), content: Faker::Lorem.sentence(3, true))
+  Product.find_by_id(rand(Product.first.id..Product.last.id)).bids.create!(user: User.find_by_id(rand(User.first.id..User.last.id)), amount: Faker::Number.between(1, 1000))
 end
