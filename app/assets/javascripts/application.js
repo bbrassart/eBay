873f1fac4469a3aabb0771eb6624a0ddf05abff3
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-07-23 19:20:47 +0200

    V.3.0 eBay Makeover. Polished the UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 
 //= require_tree .
+
