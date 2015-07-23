2275574a6c8c480bf5fce0481ffdd9cbaecf8ca8
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-07-23 11:50:35 +0200

    V.1.1 Modified the seeds file so that users and products are generated automatically

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'awesome_print'
 
 gem 'pg'
 
+gem 'faker'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+
 puts " --- Deleting all Users --- "
-# this does also delete the Entries --> dependent: :destroy
 User.destroy_all
 puts " --- Deleting all Products (MUHAHAHAHAHAA) --- "
-# this does also delete the Entries --> dependent: :destroy
-
-# rubocop:enable all
+Product.destroy_all
 
 
-def create_products_for(user)
-  Product.create!(
-    user:  user,    # yes, we can pass in the object instead of the id
-    title: 'blablabalba',
-    description: 'description',
-    deadline:  rand(0..59)
-  )
+user1 = User.create!(name: Faker::Name.name, email: Faker::Internet.free_email, password: '123', password_confirmation: '123')
+user2 = User.create!(name: Faker::Name.name, email: Faker::Internet.free_email, password: '123', password_confirmation: '123')
+user3 = User.create!(name: Faker::Name.name, email: Faker::Internet.free_email, password: '123', password_confirmation: '123')
+
+3.times do |product|
+user1.products.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
 end
 
-
-25.times do |number|
-  user = User.create!(
-    name: "User #{number}",
-    email: "Email #{number}"
-  )
-
-  rand(1..6).times do
-    create_products_for(user)
-  end
+3.times do |product|
+user2.products.create!(title: Faker::Lorem.word, description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
 end
 
+3.times do |product|
+user3.products.create!(title: Faker::Lorem.words(2), description: Faker::Lorem.sentence(3, true), deadline: Faker::Time.forward(800, :morning))
+end
